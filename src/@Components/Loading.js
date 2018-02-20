dd4f20a49677ccfb8b6a3e0e7e13bd7ca50be2e5
Simon Márton <simon.marton@gmail.com>
2018-02-20 20:51:40 +0100

    feat(Loading): Updated loading style

--- src/@Components/Loading.js
+++ src/@Components/Loading.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import sample from 'lodash.sample';
+
 import { withStyles } from 'material-ui/styles';
 import { CircularProgress } from 'material-ui/Progress';
 
-const styles = theme => ({
+const styles = ({ palette }) => ({
   container: {
     display: 'flex',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: theme.palette.grey.A100
+    color: palette.common.black,
+    background: `radial-gradient(rgba(255,255, 255, 0.5), transparent) ${sample(
+      palette.projectColors
+    )}`
   }
 });
 
 const Loading = ({ classes }) => (
   <div className={classes.container}>
-    <CircularProgress size={100} color="primary" thickness={1} />
+    <CircularProgress size={100} color="inherit" thickness={1} />
   </div>
 );
 
