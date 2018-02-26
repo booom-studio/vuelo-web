76cc77d1d9d2fe6a1e76c7ef4daa71818a814fa9
Simon Márton <simon.marton@gmail.com>
2018-02-26 12:18:01 +0100

    refact(graphql): Separated queries.

--- src/@Containers/App/index.js
+++ src/@Containers/App/index.js
@@ -1,34 +1,10 @@
 import { connect } from 'react-redux';
 
-import { graphql, compose } from 'react-apollo';
-import gql from 'graphql-tag';
+import { compose } from 'react-apollo';
 
 import { initialize, signIn, signOut } from '@dux/actions';
+import { refreshToken } from '@dux/queries';
 import App from './App';
-
-const refreshToken = graphql(
-  gql`
-    mutation($oldToken: String!) {
-      refreshToken(oldToken: $oldToken) {
-        token
-        user {
-          id
-          name
-          email
-          picture
-        }
-      }
-    }
-  `,
-  {
-    props: ({ mutate }) => ({
-      refreshToken: oldToken =>
-        mutate({ variables: { oldToken } }).then(
-          ({ data: { refreshToken } }) => refreshToken
-        )
-    })
-  }
-);
 
 const mapStateToProps = ({ initialized, user, signedIn }) => ({
   initialized,
--- src/@dux/queries.js
+++ src/@dux/queries.js
@@ -1,20 +1,140 @@
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
-const ALL_PROJECTS_QUERY = gql`
-  {
-    allProjects {
-      id
-      title
-      color
+export const projects = graphql(
+  gql`
+    {
+      allProjects {
+        id
+        title
+        color
+      }
     }
+  `,
+  {
+    props: ({ data: { allProjects: projects = [], loading, refetch } }) => ({
+      projects,
+      refetchProjects: refetch,
+      loading
+    })
   }
-`;
+);
 
-export const projects = graphql(ALL_PROJECTS_QUERY, {
-  props: ({ data: { allProjects: projects = [], loading, refetch } }) => ({
-    projects,
-    refetchProjects: refetch,
-    loading
-  })
-});
+export const createProject = graphql(
+  gql`
+    mutation($title: String!) {
+      createProject(title: $title) {
+        id
+        title
+        color
+      }
+    }
+  `,
+  {
+    props: ({ mutate }) => ({
+      createProject: ({ title }) =>
+        mutate({
+          variables: { title }
+        }).then(({ data: { createProject } }) => createProject)
+    })
+  }
+);
+
+export const updateProject = graphql(
+  gql`
+    mutation($id: ID!, $title: String, $color: String) {
+      updateProject(id: $id, title: $title, color: $color) {
+        id
+        title
+        color
+      }
+    }
+  `,
+  {
+    props: ({ mutate }) => ({
+      updateProject: ({ id, title, color }) =>
+        mutate({
+          variables: { id, title, color }
+        })
+    })
+  }
+);
+
+export const toggleProject = graphql(
+  gql`
+    mutation($projectId: ID!) {
+      toggle(projectId: $projectId) {
+        id
+        projectId
+        startTime
+        endTime
+      }
+    }
+  `,
+  {
+    props: ({ mutate }) => ({
+      toggleProject: projectId =>
+        mutate({
+          variables: { projectId }
+        }).then(({ data: { toggle } }) => toggle)
+    })
+  }
+);
+
+export const currentTimeEntry = graphql(
+  gql`
+    {
+      currentTimeEntry {
+        id
+        projectId
+        startTime
+      }
+    }
+  `,
+  {
+    props: ({ data: { currentTimeEntry, startPolling, stopPolling } }) => ({
+      currentTimeEntry,
+      currentTimeEntryPolling: {
+        start: startPolling,
+        stop: stopPolling
+      }
+    })
+  }
+);
+
+export const refreshToken = graphql(
+  gql`
+    mutation($oldToken: String!) {
+      refreshToken(oldToken: $oldToken) {
+        token
+        user {
+          id
+          name
+          email
+          picture
+        }
+      }
+    }
+  `,
+  {
+    props: ({ mutate }) => ({
+      refreshToken: oldToken =>
+        mutate({ variables: { oldToken } }).then(
+          ({ data: { refreshToken } }) => refreshToken
+        )
+    })
+  }
+);
+
+export const colors = graphql(
+  gql`
+    {
+      colors
+    }
+  `,
+  {
+    props: ({ data: { colors } }) => ({
+      colors: colors && JSON.parse(colors)
+    })
+  }
+);
